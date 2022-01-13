a03c3269fc34692f56a0666a1a76fdb850d0dd1b
bee <rkddbtjd123@gmail.com>
2022-01-13 18:51:19 +0900

    feat: user-service 에 welcome api 추가

--- components/user-component/src/main/java/me/study/springcloud/UserApi.java
+++ components/user-component/src/main/java/me/study/springcloud/UserApi.java
@@ -1,8 +1,20 @@
 package me.study.springcloud;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user-service")
 public class UserApi {
+
+    @Value("${greeting.message}")
+    private String greetingMessage;
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return greetingMessage;
+    }
 
 }
