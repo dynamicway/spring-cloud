9f4f55e5f24a97c989615a2564d47b3b7ef163a7
bee <rkddbtjd123@gmail.com>
2022-01-17 00:48:42 +0900

    feat: UserEntity 에서 비밀번호 제거

--- components/user-component/src/main/java/me/study/springcloud/user/UserEntity.java
+++ components/user-component/src/main/java/me/study/springcloud/user/UserEntity.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Getter
@@ -13,15 +14,13 @@ import javax.persistence.Id;
 public class UserEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String email;
     private String name;
-    private String password;
 
-    public UserEntity(String email, String name, String password) {
+    public UserEntity(String email, String name) {
         this.email = email;
         this.name = name;
-        this.password = password;
     }
 }
