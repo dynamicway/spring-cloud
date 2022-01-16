500b4a07735bda5966654d06777c28b3da64e292
bee <rkddbtjd123@gmail.com>
2022-01-17 00:48:52 +0900

    feat: UserRepository::findByEmail 추가

--- components/user-component/src/main/java/me/study/springcloud/user/UserRepository.java
+++ components/user-component/src/main/java/me/study/springcloud/user/UserRepository.java
@@ -2,5 +2,8 @@ package me.study.springcloud.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
 }
