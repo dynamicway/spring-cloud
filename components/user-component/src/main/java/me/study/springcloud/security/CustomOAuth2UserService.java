6b4149bee568d428eaa5923046a27e7d53c16d8a
bee <rkddbtjd123@gmail.com>
2022-01-17 00:49:41 +0900

    feat: CustomOAuth2UserService 구현

--- components/user-component/src/main/java/me/study/springcloud/security/CustomOAuth2UserService.java
+++ components/user-component/src/main/java/me/study/springcloud/security/CustomOAuth2UserService.java
@@ -1,19 +1,47 @@
 package me.study.springcloud.security;
 
 import lombok.RequiredArgsConstructor;
+import me.study.springcloud.user.UserEntity;
+import me.study.springcloud.user.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
+    private final DefaultOAuth2UserService defaultOAuth2UserService;
+    private final UserRepository userRepository;
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        return null;
+        OAuth2User oAuth2User = defaultOAuth2UserService.loadUser(userRequest);
+        getOrSaveUser(oAuth2User);
+
+        return new DefaultOAuth2User(List.of(new SimpleGrantedAuthority("ROLE_USER")),
+                oAuth2User.getAttributes(),
+                "sub"
+        );
+    }
+
+    private UserEntity getOrSaveUser(OAuth2User oAuth2User) {
+        String name = oAuth2User.getAttribute("name");
+        String email = oAuth2User.getAttribute("email");
+        return userRepository.findByEmail(email).orElse(
+                userRepository.save(new UserEntity(
+                                email,
+                                name
+                        )
+                )
+        );
     }
 
 }
